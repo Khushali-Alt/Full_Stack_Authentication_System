e6c645d531436f5c15b8697ba884f5ff2cd6c808
Khushali-Alt <ktg552026@gmail.com>
2026-09-02 17:04:30 +0530

    feat: implement user signup with MongoDB and bcrypt password hashing

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { Axios } from "axios";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 
 
 
 export default function Signup(){
+  const router= useRouter();
     const [user , setUser] = React.useState({
         email:"",
         password:"",
@@ -16,12 +18,49 @@ export default function Signup(){
         
     })
 
-     const onSignup=async()=>{
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
 
-     }
-     return (
+    const [loading, setLoading] = React.useState(false);
+
+     const onSignup=async()=>{
+      try{
+        setLoading(true);
+
+        const response =await axios.post("/api/users/signup", user);
+
+        console.log("signup success", response.data);
+
+        toast.success("signup success");
+
+        router.push("/login");
+
+      }catch(error:any){
+        console.log("signup failed", error.message);
+        
+        toast.error(error.message);
+          
+     }finally{
+        setLoading(false);
+      }
+    };
+
+
+     
+     //depend on the user
+     useEffect(()=>{
+      if(user.email.length>0 && user.password.length>0
+        && user.username.length>0){
+        setButtonDisabled(false);
+      }else{
+        setButtonDisabled(true);
+      }
+     }, [user]);
+
+
+
+return (
    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-  <h1 className="text-2xl font-bold mb-4">Sign Up</h1>
+  <h1 className="text-2xl font-bold mb-4 text-white">{loading ? "Processing.." : "Singup"}</h1>
 
   <hr className="w-full max-w-sm mb-6" />
 
@@ -30,8 +69,8 @@ export default function Signup(){
   </label>
 
   <input
-    className="w-80 p-2 border border-gray-300 rounded-lg mb-4 
-               focus:outline-none focus:border-gray-600"
+    className="w-80 p-2 border bg-white border-gray-300 rounded-lg mb-4 
+               focus:outline-none placeholder:text-gray-400 focus:border-gray-600 text-black "
     type="text"
     id="username"
     value={user.username}
@@ -44,8 +83,8 @@ export default function Signup(){
   </label>
 
   <input
-    className="w-80 p-2 border border-gray-300 rounded-lg mb-4 
-               focus:outline-none focus:border-gray-600"
+    className="w-80 p-2 border bg-white border-gray-300 rounded-lg mb-4 
+               focus:outline-none text-black focus:border-gray-600 placeholder:text-gray-400 "
     type="text"
     id="email"
     value={user.email}
@@ -58,8 +97,8 @@ export default function Signup(){
   </label>
 
   <input
-    className="w-80 p-2 border border-gray-300 rounded-lg mb-4 
-               focus:outline-none focus:border-gray-600"
+    className="w-80 p-2 border bg-white border-gray-300 rounded-lg mb-4 
+               focus:outline-none text-black  focus:border-gray-600 placeholder:text-gray-400 "
     type="password"
     id="password"
     value={user.password}
@@ -69,12 +108,12 @@ export default function Signup(){
 
 <button
 onClick={onSignup}
-className="w-80 p-2 border border-gray-300 rounded-lg mb-4 
-               focus:outline-none focus:border-gray-600"
-               > Signup here </button>
+className="w-80 p-2 border bg-white border-gray-300 rounded-lg mb-4 
+               focus:outline-none focus:border-gray-600 text-black"
+               > {buttonDisabled ? "No signup" : "Sign Up"}</button>
 <Link href="/login">visit login here</Link>               
   
 </div>
       
-     )
+     );
 }
